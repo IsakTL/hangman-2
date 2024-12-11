7650756babb60d50d545318a85faf4a1e162b292
Jack.Bass <jackbass1220@gmail.com>
2024-12-11 11:47:22 -0500

    changing leaderboard

---
+++ server/src/models/leaderboard.ts
@@ -0,0 +1,22 @@
+// import { User } from './User.js';
+// import { Schema, model, type Document } from 'mongoose';
+
+
+// interface ILeaderboard extends Document {
+//     numofGamesWon: number; 
+//     UserId: Schema.Types.ObjectId; // Array of Players
+//     guesses: string[]; 
+// }
+
+// const leaderboardSchema: Schema<ILeaderboard> = new Schema ( 
+//     {
+//         numofGamesWon: {
+//             type: Number, 
+//         }, 
+//         UserId: {
+//             type: String, 
+//         }, 
+//     },  
+// ); 
+
+// leaderboardSchema
